21dacffd0cf264fd318edd1a1c622eabfad495f3
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2020-08-13 15:11:46 +0530

    Added the main application and updated the README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MS Teams - Meeting Attendance Compiler
+# Attendance Compiler for MS Teams
+
+## Why
+The generated attendance file from Microsoft Teams simply provides a log of the participants joining and leaving the meeting, along with their associated timestamps, as a CSV.  
+&nbsp;  
+For a classroom setting, however, it is often required to track a student's time-of-entry into the classroom, as well as their attendance duration, in order to determine whether credit for attendance should be disbursed or not.  
+&nbsp;  
+Similar use-cases may exist beyond the classroom.
+
+## Usage
+1. From a Microsoft Teams meeting, download the attendance CSV file from the Participants sidebar
+2. Visit this project's website, and set the configuration parameters
+3. Drag-and-drop or click-to-upload the attendance file
+4. The aggregated attendance file will then be downloaded
---
+++ main.js
@@ -0,0 +1,245 @@
+function dropHandler(ev) {
+    console.log("dropHandler")
+    let file;
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+
+    if (ev.dataTransfer.items) {
+        // Use DataTransferItemList interface to access the file(s)
+        if (ev.dataTransfer.items[0].kind === 'file') {
+            file = ev.dataTransfer.items[0].getAsFile();
+            console.log(file);
+        }
+    } else {
+        // Use DataTransfer interface to access the file(s)
+        file = ev.dataTransfer.files[0];
+        console.log(file);
+    }
+
+    if (typeof file !== `undefined`) {
+        if (file.type === `text/csv`) {
+            initCSV(file);
+        }
+    }
+}
+
+function dragOverHandler(ev) {
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+}
+
+function getHoursMins(timeString) {
+    let split1 = timeString.split(" ");
+    let ampm = split1[1];
+    let split2 = split1[0].split(":");
+    let hours = parseInt(split2[0]);
+    let minutes = parseInt(split2[1]);
+    if (ampm === 'AM') {
+        if (hours === 12) {
+            hours = 0;
+        }
+    } else {
+        if (hours !== 12) {
+            hours += 12;
+        }
+    }
+    return { hours, minutes };
+}
+
+function initCSV(file) {
+    let userInputs = {
+        startTime: getHoursMins($("#startTimePicker").val()),
+        endTime: getHoursMins($("#endTimePicker").val()),
+        threshold: $("#thresholdPicker").val()
+    }
+    
+    let classDuration = (userInputs.endTime.hours * 60 + userInputs.endTime.minutes) - (userInputs.startTime.hours * 60 + userInputs.startTime.minutes);
+    if (classDuration <= 0) {
+        alert('Configuration error. The End Time must be greater than the Start Time.')
+    } else if(userInputs.threshold > classDuration) {
+        alert('Configuration error. The Attendance Requirement cannot be greater than the class duration.')
+    } else {
+        readCSV(file).then((obj) => {
+            analyzeCSV(obj, userInputs).then((results) => {
+                console.log(results);
+                downloadCSV(results);
+            }).catch(() => {
+                alert('There was an error. Please check your inputs and try again.');
+            });
+        }).catch(function () {
+            alert('Your browser is not compatible.');
+        });
+    }
+}
+
+function readCSV(file) {
+    return new Promise(function (resolve, reject) {
+        if (window.FileReader) {
+            // FileReader is supported
+            var reader = new FileReader();
+            reader.readAsText(file);
+            reader.onload = function (event) {
+                let csvContent = event.target.result;
+                console.log(csvContent);
+                let csvObjects = $.csv.toObjects(csvContent);
+                console.log(csvObjects);
+                resolve(csvObjects);
+            }
+        } else {
+            reject();
+        }
+    });
+}
+
+function analyzeCSV(data, inputs) {
+    return new Promise((resolve, reject) => {
+        try {
+            let classStartTime = new Date(data[0][`Timestamp`]);
+            classStartTime.setHours(inputs.startTime.hours);
+            classStartTime.setMinutes(inputs.startTime.minutes);
+            let classEndTime = new Date(classStartTime);
+            classEndTime.setHours(inputs.endTime.hours);
+            classEndTime.setMinutes(inputs.endTime.minutes);
+
+            let attendanceThreshold = inputs.threshold;
+            console.log(classStartTime);
+            console.log(classEndTime);
+
+            let prevName = null;
+            let joiningTime = null;
+            let startTime = null, endTime = new Date(classEndTime);
+            let duration = 0;
+            let skipRecord = true;
+
+            let resultCSV = [];
+
+            data.forEach(function (row) {
+                rowFullName = row[`Full Name`];
+                rowUserAction = row[`User Action`];
+                rowTimestamp = row[`Timestamp`];
+
+                if (!([`Unknown User`].includes(rowFullName))) { // Name exceptions
+                    if (rowFullName !== prevName) {
+                        let firstStudent = false;
+                        // New student
+                        if (skipRecord && prevName !== null) {
+                            // First non-teacher record
+                            skipRecord = false;
+                            firstStudent = true;
+                        }
+                        if (!skipRecord) {
+                            if (!firstStudent) {
+                                // Store attendance for the previous student in the result
+                                duration += (endTime - startTime) / (1000 * 60)
+                                resultCSV.push({
+                                    'Name': prevName,
+                                    'Joining Time': joiningTime.toLocaleString(),
+                                    'Minutes Attended': Math.ceil(duration),
+                                    'Attendance': (Math.ceil(duration) >= attendanceThreshold) ? 'P' : 'A'
+                                });
+                            }
+
+                            // Reset values
+                            endTime = new Date(classEndTime);
+                            duration = 0;
+
+                            if (rowUserAction === `Joined`) {
+                                if (new Date(rowTimestamp) < new Date(classStartTime)) {
+                                    joiningTime = new Date(classStartTime);
+                                    startTime = new Date(classStartTime);
+                                } else {
+                                    joiningTime = new Date(rowTimestamp);
+                                    startTime = new Date(rowTimestamp);
+                                }
+                                // duration = (endTime - startTime) / (1000 * 60); // In minutes
+                            } else if (rowUserAction === `Joined before`) {
+                                joiningTime = new Date(classStartTime);
+                                startTime = new Date(classStartTime);
+                                // duration = (endTime - startTime) / (1000 * 60); // In minutes
+                            }
+                        }
+                        prevName = rowFullName;
+                    } else {
+                        // Same student
+                        if (!skipRecord) {
+                            if (rowUserAction === `Joined before`) {
+                                joiningTime = new Date(classStartTime);
+                                startTime = new Date(classStartTime);
+                                endTime = new Date(classEndTime);
+                                // duration = (endTime - startTime) / (1000 * 60); // In minutes
+                            } else if (rowUserAction === `Left`) {
+                                if (new Date(rowTimestamp) < joiningTime) {
+                                    // duration = 0;
+                                    endTime = new Date(joiningTime);
+                                } else {
+                                    endTime = new Date(rowTimestamp);
+                                }
+                            } else if (rowUserAction === `Joined`) {
+                                if (new Date(rowTimestamp) < new Date(classStartTime)) {
+                                    endTime = new Date(classEndTime);
+                                } else {
+                                    duration += (endTime - startTime) / (1000 * 60)
+                                    startTime = new Date(rowTimestamp);
+                                    endTime = new Date(classEndTime);
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+            resolve(resultCSV);
+        } catch (err) {
+            reject();
+        }
+    });
+}
+
+function downloadCSV(obj) {
+    let csvResult = "data:text/csv;charset=utf-8," + $.csv.fromObjects(obj);
+    // obj.forEach(function (rowArray) {
+    //     let row = rowArray.join(",");
+    //     csvResult += row + "\r\n";
+    // });
+    encodedUri = encodeURI(csvResult);
+    let link = document.createElement("a");
+    link.setAttribute("href", encodedUri);
+    link.setAttribute("download", "my_data.csv");
+    document.body.appendChild(link);
+    link.click();
+}
+
+$(document).ready(function () {
+    let defaultStartTime = '8:30';
+    let defaultEndTime = '9:30';
+    let defaultThreshold = 50;
+
+    let timepickerOptions = {
+        timeFormat: 'h:mm p',
+        interval: 15,
+        dropdown: true,
+        dynamic: false,
+        scrollbar: true
+    };
+
+    $("#startTimePicker").timepicker(Object.assign({}, timepickerOptions, {
+        defaultTime: defaultStartTime,
+        startTime: defaultStartTime
+    }));
+    $("#endTimePicker").timepicker(Object.assign({}, timepickerOptions, {
+        defaultTime: defaultEndTime,
+        startTime: defaultEndTime
+    }));
+    $("#thresholdPicker").val(defaultThreshold);
+
+    $('#dropZone').click(function () { $('#csvFile').trigger('click'); });
+
+    $("#csvFile").on('change', function (data) {
+        console.log('csvFileChange');
+        console.log(data);
+        let file = $(data.target)[0].files[0];
+        console.log(file);
+        if (file.type === `text/csv`) {
+            initCSV(file);
+        }
+    });
+});
